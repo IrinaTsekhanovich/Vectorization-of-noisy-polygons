c52a60d1f8c8ae079d1552eff87412bc68241e4f
Irina <ira.tsekhanovich@yandex.ru>
2020-06-04 15:58:34 +0300

    add gmsh model

--- vectorization.py
+++ vectorization.py
@@ -3,7 +3,9 @@ import math as m
 import cv2
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
-
+import gmsh
+from collections import deque
+from PIL import Image, ImageDraw
 
 def findcontours_suzuki(data, symbol):
     img = []
@@ -11,12 +13,185 @@ def findcontours_suzuki(data, symbol):
         img.append([])
         for j in range(len(data[0]) - 1):
             if data[i][j] == symbol:
-                img[i].append(255)
+                if not (not (j > 1 and j < len(data[0]) - 1 and data[i][j + 1] != symbol and data[i][
+                    j - 1] != symbol) and not (
+                        i > 1 and i < len(data) - 1 and data[i + 1][j] != symbol and data[i - 1][j] != symbol)):
+                    img[i].append(0)
+                else:
+                    img[i].append(255)
             else:
                 img[i].append(0)
     image = np.uint8(img)
     contours, hierarchy = cv2.findContours(image, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
     return contours
+
+
+def isCNV_old(data, i, j):
+    if i == 0 or j == 0 or i == len(data) - 1 or j == len(data[0]) - 2 or (
+            (j + 1 < len(data[0]) - 1 and data[i][j + 1] != data[i][j]) or
+            (j + 1 < len(data[0]) - 1 and i - 1 >= 0 and data[i - 1][j + 1] != data[i][j]) or
+            (j + 1 < len(data[0]) - 1 and i + 1 < len(data) and data[i + 1][j + 1] != data[i][j]) or
+            (i + 1 < len(data) and data[i + 1][j] != data[i][j]) or
+            (i + 1 < len(data) and j - 1 >= 0 and data[i + 1][j - 1] != data[i][j]) or
+            (j - 1 >= 0 and data[i][j - 1] != data[i][j]) or
+            (j - 1 >= 0 and i - 1 >= 0 and data[i - 1][j - 1] != data[i][j]) or
+            (i - 1 >= 0 and data[i - 1][j] != data[i][j])):
+        return True
+    else:
+        return False
+
+def find_neighbour(S,i,j):
+    if S == 0:
+        return i, j+1
+    if S == 1:
+        return i-1, j+1
+    if S == 2:
+        return i-1, j
+    if S == 3:
+        return i-1, j-1
+    if S == 4:
+        return i, j-1
+    if S == 5:
+        return i+1, j-1
+    if S == 6:
+        return i+1, j
+    if S == 7:
+        return i+1, j+1
+
+
+def findcontours_teo(start_point, img, symbol):
+    first = True
+    current_point = start_point
+    S = 6
+    cnv = [start_point]
+    while (first or current_point != start_point):
+        found = False
+        while not found:
+            if S == 0:
+                i1, j1 = find_neighbour(7, current_point[0], current_point[1])
+            else:
+                i1, j1 = find_neighbour((S - 1) % 8, current_point[0], current_point[1])
+            i2, j2 = find_neighbour(S, current_point[0], current_point[1])
+            i3, j3 = find_neighbour((S + 1) % 8, current_point[0], current_point[1])
+            if img[i1][j1] == symbol or img[i1][j1] >= 3:
+                found = True
+                cnv.append((i1, j1))
+                if S == 0:
+                    S = 6
+                elif S == 1:
+                    S = 7
+                else:
+                    S = (S - 2) % 8
+                if img[i1][j1] >= 3:
+                    img[i1][j1] += 1
+                else:
+                    img[i1][j1] = 3
+                current_point = (i1, j1)
+            elif img[i2][j2] == symbol or img[i2][j2] >= 3:
+                found = True
+                cnv.append((i2, j2))
+                if img[i2][j2] >= 3:
+                    img[i2][j2] += 1
+                else:
+                    img[i2][j2] = 3
+                current_point = (i2, j2)
+            elif img[i3][j3] == symbol or img[i3][j3] >= 3:
+                found = True
+                cnv.append((i3, j3))
+                if img[i3][j3] >= 3:
+                    img[i3][j3] += 1
+                else:
+                    img[i3][j3] = 3
+                current_point = (i3, j3)
+            else:
+                S = (S + 2) % 8
+        first = False
+    return cnv
+
+
+def find_contours_teo_bis(data, symbol):
+    img = []
+    for i in range(len(data)):
+        img.append([])
+        for j in range(len(data[0])-1):
+            if data[i][j] == symbol:
+                img[i].append(1)
+            else:
+                img[i].append(0)
+    cnv = []
+    for i in range(len(img)):
+        for j in range(len(img[0])):
+            if img[i][j] == 1 and j - 1 >= 0 and img[i][j - 1] == 0:
+                start_point = (i, j)
+                cnv.append(findcontours_teo(start_point, img, 1))
+    return cnv
+
+
+def findcnv(data, i, j, symbol, flags):
+    object = []
+    q = deque()
+    flags.add((i, j))
+    object.append([j, i])
+    q.append((i, j))
+    while q:
+        current = q.popleft()
+        i_cur = current[0]
+        j_cur = current[1]
+        if j_cur + 1 < len(data[0]) - 1 and isCNV_old(data, i_cur, j_cur + 1) and not (i_cur, j_cur + 1) in flags:
+            object.append([j_cur + 1, i_cur])
+            flags.add((i_cur, j_cur + 1))
+            q.append((i_cur, j_cur + 1))
+            continue
+        if i_cur + 1 < len(data) and isCNV_old(data, i_cur + 1, j_cur) and not (i_cur + 1, j_cur) in flags:
+            object.append([j_cur, i_cur + 1])
+            flags.add((i_cur + 1, j_cur))
+            q.append((i_cur + 1, j_cur))
+            continue
+        if j_cur - 1 >= 0 and isCNV_old(data, i_cur, j_cur - 1) and not (i_cur, j_cur - 1) in flags:
+            object.append([j_cur - 1, i_cur])
+            flags.add((i_cur, j_cur - 1))
+            q.append((i_cur, j_cur - 1))
+            continue
+        if i_cur - 1 >= 0 and isCNV_old(data, i_cur - 1, j_cur) and not (i_cur - 1, j_cur) in flags:
+            object.append([j_cur, i_cur - 1])
+            flags.add((i_cur - 1, j_cur))
+            q.append((i_cur - 1, j_cur))
+            continue
+        if i_cur + 1 < len(data) and j_cur - 1 >= 0 and isCNV_old(data, i_cur + 1, j_cur - 1) and not (i_cur + 1,
+                                                                                                       j_cur - 1) in flags:
+            object.append([j_cur - 1, i_cur + 1])
+            flags.add((i_cur + 1, j_cur - 1))
+            q.append((i_cur + 1, j_cur - 1))
+            continue
+        if i_cur + 1 < len(data) and j_cur + 1 < len(data[0]) - 1 and isCNV_old(data, i_cur + 1, j_cur + 1) and not (
+                                                                                                                    i_cur + 1,
+                                                                                                                    j_cur + 1) in flags:
+            object.append([j_cur + 1, i_cur + 1])
+            flags.add((i_cur + 1, j_cur + 1))
+            q.append((i_cur + 1, j_cur + 1))
+            continue
+        if i_cur - 1 >= 0 and j_cur - 1 >= 0 and isCNV_old(data, i_cur - 1, j_cur - 1) and not (i_cur - 1,
+                                                                                                j_cur - 1) in flags:
+            object.append([j_cur - 1, i_cur - 1])
+            flags.add((i_cur - 1, j_cur - 1))
+            q.append((i_cur - 1, j_cur - 1))
+            continue
+        if i_cur - 1 >= 0 and j_cur + 1 < len(data[0]) - 1 and isCNV_old(data, i_cur - 1, j_cur + 1) and not (i_cur - 1,
+                                                                                                              j_cur + 1) in flags:
+            object.append([j_cur + 1, i_cur - 1])
+            flags.add((i_cur - 1, j_cur + 1))
+            q.append((i_cur - 1, j_cur + 1))
+            continue
+    return object
+
+
+def findcntrs():
+    cnv = [[], []]
+    flags = set()
+    for i in range(len(data)):
+        for j in range(len(data[0]) - 1):
+            if data[i][j] != '0' and isCNV_old(data, i, j) and not (i, j) in flags:
+                cnv[int(data[i][j]) - 1].append(findcnv(data, i, j, data[i][j], flags))
 
 
 def distance(p1, p2):
@@ -215,18 +390,23 @@ def fun(x):
     return loss
 
 
-def create_rctngl_new(cnv, appr):
+def create_rctngl_new(cnv1, appr):
+    cnv=[]
+    for c in cnv1:
+        cnv.append([[c[0][0], sizey - c[0][1]]])
     points = get_iters(appr, cnv)
+
+    points.sort()
     bounds0 = [[], [], [], []]
-    for i in range(points[3][0] + 1, len(cnv)):
+    for i in range(points[3][0], len(cnv)):
         bounds0[0].append([cnv[i][0][0], cnv[i][0][1]])
     for i in range(points[0][0]):
         bounds0[0].append([cnv[i][0][0], cnv[i][0][1]])
-    for i in range(points[0][0] + 1, points[1][0]):
+    for i in range(points[0][0]+1, points[1][0]):
         bounds0[1].append([cnv[i][0][0], cnv[i][0][1]])
-    for i in range(points[1][0] + 1, points[2][0]):
+    for i in range(points[1][0]+1, points[2][0]):
         bounds0[2].append([cnv[i][0][0], cnv[i][0][1]])
-    for i in range(points[2][0] + 1, points[3][0]):
+    for i in range(points[2][0]+1, points[3][0]):
         bounds0[3].append([cnv[i][0][0], cnv[i][0][1]])
 
     for i in range(4):
@@ -234,6 +414,7 @@ def create_rctngl_new(cnv, appr):
 
     first_line = create_line([points[0][1],points[0][2]], [points[1][1],points[1][2]])
     l0 = [first_line[0], first_line[1], first_line[2], 1, 1, 1]
+    # l0 = [1,1,1,1,1,1]
     coef = minimize(fun, l0, method='BFGS')
     lines = []
     lines.append((coef['x'][0], coef['x'][1], coef['x'][2]))
@@ -247,6 +428,10 @@ def create_rctngl_new(cnv, appr):
     return res_points
 
 
+def findY(line, x):
+    return -(x[0]*line[0] + line[2])/line[1]
+
+
 def create_line(p1, p2):
     x1 = p1[0]
     x2 = p2[0]
@@ -258,10 +443,20 @@ def create_line(p1, p2):
     return [A, B, C]
 
 
+def find_angle(p1, p2):
+    return m.atan((p2[1]-p1[1])/(p2[0]-p1[0]))
+
+
 data = []
-with open('segment.txt', 'r') as f:
+i = -1
+with open('audi/000001_Audi_Run5.txt', 'r') as f:
     for line in f.readlines():
-        data.append(line)
+        if len(line)>1:
+            i += 1
+            data.append([])
+            for c in line:
+                if c!=',':
+                    data[i].append(c)
 sizey = len(data) - 1
 
 cnv = [findcontours_suzuki(data, '1'), findcontours_suzuki(data, '2')]
@@ -279,7 +474,7 @@ appr_masood_second = []
 for c in appr_masood_2:
     appr_masood_second.append([c[0], sizey - c[1]])
 
-
+#points 0,3 - big circle arc;1,2 - small circle arc
 tmp = [[0,appr_masood_second[0][0],appr_masood_second[0][1]],[0,appr_masood_second[0][0],appr_masood_second[0][1]],
        [0,appr_masood_second[0][0],appr_masood_second[0][1]],[0,appr_masood_second[0][0],appr_masood_second[0][1]]]
 for i in range(len(appr_masood_second)):
@@ -287,11 +482,10 @@ for i in range(len(appr_masood_second)):
         tmp[0] = [i,appr_masood_second[i][0], appr_masood_second[i][1]]
     if appr_masood_second[i][0] <= tmp[1][1] and appr_masood_second[i][1] < tmp[1][2]:
         tmp[1] = [i,appr_masood_second[i][0], appr_masood_second[i][1]]
-    if appr_masood_second[i][0] >= tmp[2][1] and appr_masood_second[i][1] < tmp[2][2]:
+    if appr_masood_second[i][0] > tmp[2][1] and appr_masood_second[i][1] <= tmp[2][2]:
         tmp[2] = [i,appr_masood_second[i][0], appr_masood_second[i][1]]
-    if appr_masood_second[i][0] >= tmp[3][1]:
+    if appr_masood_second[i][0] >= tmp[3][1] and appr_masood_second[i][1] >= tmp[3][2]:
         tmp[3] = [i,appr_masood_second[i][0], appr_masood_second[i][1]]
-
 
 
 smth = []
@@ -306,20 +500,25 @@ for i in range(tmp[2][0]+1, tmp[3][0]):
 
 line0 = create_line([tmp[0][1],tmp[0][2]], [tmp[1][1],tmp[1][2]])
 line1 = create_line([tmp[2][1],tmp[2][2]], [tmp[3][1],tmp[3][2]])
-line2 = create_line(smth[0],smth[1])
-line3 = create_line(smth[len(smth)-1], smth[len(smth)-2])
+cntr = cross(line0, line1) # centre of circle
+
+angle = find_angle([tmp[2][1], tmp[2][2]], [tmp[3][1], tmp[3][2]])
+tmp[2][1] = (tmp[1][2]-cntr[1])*m.cos(angle)+cntr[0]
+tmp[2][2] = (tmp[1][2]-cntr[1])*m.sin(angle)+cntr[1]
+
+line1 = create_line([tmp[2][1],tmp[2][2]], [tmp[3][1],tmp[3][2]])
 point1 = smth[0]
 point2 = smth[len(smth)-1]
 
 
-while (smth[0][0] >= cross(line1, line2)[0]):
+while(smth[0][1] <= findY(line1, smth[0])):
     point1 = smth[0]
     smth.pop(0)
-    line2 = create_line(smth[0],smth[1])
-while (smth[len(smth)-1][0] >= cross(line1, line3)[0]):
+while(smth[len(smth)-1][1] <= findY(line1, smth[len(smth)-1])):
     point2 = smth[len(smth)-1]
     smth.pop(len(smth)-1)
-    line3 = create_line(smth[len(smth)-1], smth[len(smth)-2])
+line2 = create_line(smth[0], point1)
+line3 = create_line(smth[len(smth)-1], point2)
 
 if point1 == smth[0]:
     line2 = create_line(smth[0], smth[1])
@@ -333,15 +532,50 @@ else:
 smth.insert(0, cross(line1, line2))
 smth.append(cross(line1, line3))
 res_points.append(smth)
-
-res_points.append([cross(line0, line1)]) # centre of circle
-res_points.append([[tmp[i][1], tmp[i][2]] for i in range(len(tmp))]) #points 0,3 - big circle arc;1,2 - small circle arc
 for i in range(len(cnv[1])):
     bounds = []
-    coolpoints = create_rctngl_new(cnv[1][i], masood(cnv[1][i], False, 4))
+    tmp_appr_2 = []
+    tmp_appr = masood(cnv[1][i], False, 4)
+    for c in tmp_appr:
+        tmp_appr_2.append([c[0], sizey - c[1]])
+
+    coolpoints = create_rctngl_new(cnv[1][i], tmp_appr_2)
     res_points.append([])
     for j in range(len(coolpoints)):
-        res_points[len(res_points)-1].append([coolpoints[j][0],sizey - coolpoints[j][1]])
+        res_points[len(res_points)-1].append([coolpoints[j][0], coolpoints[j][1]])
+
+model = gmsh.model
+factory = model.geo
+
+gmsh.initialize()
+gmsh.option.setNumber("General.Terminal", 1)
+
+model.add("rotor")
+lc = 1e-2
+
+factory.addPoint(cntr[0], cntr[1], 0, lc, 1)
+for i in range(4):
+    factory.addPoint(tmp[i][1], tmp[i][2], 0, lc, i+2)
+factory.addLine(2, 3, 1)
+factory.addCircleArc(3, 1, 4, 2)
+factory.addLine(4, 5, 3)
+factory.addCircleArc(5, 1, 2, 4)
+
+point_cnt = 5
+for i in range(len(res_points)):
+    for j in range(len(res_points[i])):
+        point_cnt += 1
+        factory.addPoint(res_points[i][j][0], res_points[i][j][1], 0, lc, point_cnt)
+    start = point_cnt - len(res_points[i]) + 1
+    for j in range(len(res_points[i])-1):
+        factory.addLine(start+j, start+j+1)
+    factory.addLine(point_cnt, start)
+
+factory.synchronize()
+model.mesh.generate(2)
+
+gmsh.write("audi/1.geo_unrolled")
+gmsh.finalize()
 
 
 #подсчет ошибок
